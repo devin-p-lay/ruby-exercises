c89de93791995b11117d5463263937e0cf45e6ed
Devin Pile <devinpile@gmail.com>
2022-06-29 19:13:08 -0600

    Add changes for unicorn

--- mythical-creatures/lib/unicorn.rb
+++ mythical-creatures/lib/unicorn.rb
@@ -3,7 +3,21 @@ class Unicorn
               :color,
               :magical_powers
 
-  def initialize(name, color)
+  def initialize(name, color = 'silver')
     @name = name
     @color = color
     @magical_powers = []
+  end
+
+  def silver?
+    if color == 'silver'
+      true
+    else
+      false
+    end
+  end
+
+  def say(input)
+    '**;* ' + input + ' **;*'
+  end
+end
