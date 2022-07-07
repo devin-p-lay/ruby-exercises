c111fc111410501cbe530b8c88cf0faf60d5c5d4
Devin Pile <devinpile@gmail.com>
2022-07-06 18:59:55 -0600

    Add rabbit initialize

--- initialize/lib/rabbit.rb
+++ initialize/lib/rabbit.rb
@@ -1,2 +1,14 @@
 class Rabbit
-end 
+  def initialize(info)
+    @name = info[:name]
+    @num_syllables = info[:num_syllables]
+  end
+
+  def name
+    if @num_syllables == 2
+      @name + ' Rabbit'
+    else
+      @name
+    end
+  end 
+end
