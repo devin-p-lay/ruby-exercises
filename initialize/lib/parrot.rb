bf9f5a8f0cb85334231cc542fd9c053489a82610
Devin Pile <devinpile@gmail.com>
2022-07-06 18:52:09 -0600

    Add parrot initialize

--- initialize/lib/parrot.rb
+++ initialize/lib/parrot.rb
@@ -1,2 +1,11 @@
 class Parrot
+  attr_reader :name,
+              :known_words,
+              :sound
+
+  def initialize(info)
+    @name = info[:name]
+    @known_words = info[:known_words]
+    @sound = 'Squawk!'
+  end
 end
